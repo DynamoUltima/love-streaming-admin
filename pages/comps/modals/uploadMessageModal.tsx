04e7b1ee85837861948d80f27faacd651e908b4a
Joel <dynamo.joey@gmail.com>
2022-12-08 11:25:28 +0000

    upload  modal upadated

--- pages/comps/modals/uploadMessageModal.tsx
+++ pages/comps/modals/uploadMessageModal.tsx
@@ -1,6 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 interface Modal {
     isOpen: boolean,
@@ -9,11 +9,11 @@ interface Modal {
 
 const UploadMessageModal = ({ isOpen, closeModal }: Modal) => {
 
-    const {register,handleSubmit,formState:{errors}} = useForm();
-    const myFun=(d:any)=>{
-        alert('button is clicked')
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const myFun = (d: any) => {
+        alert(d.email + "from" + d.fullname)
     }
-    
+
     return (
         <>
             <Transition appear show={isOpen ?? false} as={Fragment}>
@@ -44,7 +44,7 @@ const UploadMessageModal = ({ isOpen, closeModal }: Modal) => {
                                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-grayblack p-6 text-left align-middle shadow-xl transition-all space-y-4">
                                     <Dialog.Title
                                         as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-50 py-4"
+                                        className="text-lg  leading-6 text-gray-400 py-4 font-extrabold flex flex-row items-center justify-center"
                                     >
                                         Add Message
                                     </Dialog.Title>
@@ -53,49 +53,59 @@ const UploadMessageModal = ({ isOpen, closeModal }: Modal) => {
                                     {/* <SearchBar /> */}
 
                                     <div className="max-h-64 overflow-auto space-y-4">
-                    
+
 
                                         <form onSubmit={handleSubmit(myFun)} className="w-full max-w-sm">
                                             <div className="md:flex md:items-center mb-6">
-                                                <div className="md:w-1/3 form-group">
-                                                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="email">
-                                                        Email
-                                                    </label>
-                                                </div>
-                                                <div className="md:w-2/3">
-                                                    <input {...register('email',{required:true})} className=" form-control bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"  />
-                                                    {errors.email&& errors.email.type =="required" && <p className="text-red-500">please enter email</p>}
+
+                                                <div className="w-full space-y-1">
+                                                    <label className="text-gray-500" htmlFor="title">Title</label>
+                                                    <input {...register('title', { required: true })} className=" form-control  bg-mattblack p-2 text-gray-200 focus:outline-none rounded w-full py-2 px-4 leading-tight " placeholder="Title" id="title" type="text" />
+                                                    {errors.title && errors.title.type == "required" && <p className="text-red-500">please enter title</p>}
                                                 </div>
                                             </div>
 
                                             <div className="md:flex md:items-center mb-6 form-group">
-                                                <div className="md:w-1/3">
-                                                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="fullname">
-                                                        fullname
+
+                                                <div className=" w-full space-y-1">
+                                                    <label className="text-gray-500" htmlFor="description">
+                                                        Description
                                                     </label>
-                                                </div>
-                                                <div className="md:w-2/3">
-                                                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
+                                                    <input {...register('description', { required: true, minLength: 3 })} className=" form-control  bg-mattblack p-2 text-gray-200 focus:outline-none rounded w-full py-2 px-4 leading-tight" placeholder="Description" id="fullname" type="text" />
+                                                    {errors.description && errors.description.type == "required" && <p className="text-red-500 text-sm">please enter message description</p>}
+                                                    {errors.description && errors.description.type == "minLength" && <p className="text-red-500 text-sm">please enter  a minimum of 3 letters</p>}
+
                                                 </div>
                                             </div>
 
-                                            <div className="md:flex md:items-center mb-6">
-                                                <div className="md:w-1/3"></div>
-                                                <label className="md:w-2/3 block text-gray-500 font-bold">
-                                                    <input className="mr-2 leading-tight" type="checkbox" />
-                                                    <span className="text-sm">
-                                                        Send me your newsletter!
-                                                    </span>
-                                                </label>
+
+
+                                            <div className="mt-4 flex flex-row space-x-2">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                    onClick={closeModal}
+                                                >
+                                                    Cancel
+                                                </button>
+                                                <button
+                                                    type="submit"
+                                                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                // onClick={closeModal}
+                                                >
+                                                    Save
+                                                </button>
                                             </div>
-                                            <div className="md:flex md:items-center form-group">
+
+
+                                            {/* <div className="md:flex md:items-center form-group">
                                                 <div className="md:w-1/3"></div>
                                                 <div className="md:w-2/3">
                                                     <button className="shadow form-control bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                                                         Sign Up
                                                     </button>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </form>
 
 
@@ -103,17 +113,9 @@ const UploadMessageModal = ({ isOpen, closeModal }: Modal) => {
 
 
 
-                                    <div className="mt-4">
-                                        <button
-                                            type="button"
-                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                            onClick={closeModal}
-                                        >
-                                            Got it, thanks!
-                                        </button>
-                                    </div>
 
-                                   
+
+
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
