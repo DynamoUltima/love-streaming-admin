fe534752659f62fce69f539177d2e5b514605e7b
Joel <dynamo.joey@gmail.com>
2023-01-12 11:33:11 +0000

    activated modals api key changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const fetchYoutube = async () => {
           part: "snippet,id",
           order: "date",
           maxResults: "50",
-          pageToken: ""
+          // pageToken: ""
       }
   }) ;
 
